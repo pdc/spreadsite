623b7afe1c5da7d6ee7fe62a4227515ce0de782c
pdc <pdc@alleged.org.uk>
2010-02-10 09:31:53 +0000

    Add links from the spreadsite library to the spreadsite blog page.

--- downblog/views.py
+++ downblog/views.py
@@ -1,10 +1,12 @@
 # Create your views here.
-from django.http import HttpResponse
-from django.template import RequestContext
-from django.shortcuts import render_to_response
 import os
 import re
 import email.parser
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.conf import settings
+from spreadsite.spreadlinks.models import get_library_or_404
 
 # Date followed by slug.
 entry_file_name_re = re.compile(r'^(199[0-9]|20[0-9][0-9])-([0-1][0-9])-([0-3][0-9])[._-]?([a-zA-Z0-9-]*)\.(e|md)$')
@@ -12,6 +14,9 @@ entry_file_name_re = re.compile(r'^(199[0-9]|20[0-9][0-9])-([0-1][0-9])-([0-3][0
 parser = email.parser.Parser()
 
 def entry_list(request, root_dir):
+    # Get list of entries
+    # TODO. Split in to separate function.
+    # TODO. Cache
     entries = []
     for dir_name, subdirs, files in os.walk(root_dir):
         for file_name in files:
@@ -20,6 +25,9 @@ def entry_list(request, root_dir):
                 entries.append(m.groups() + (dir_name, file_name))
     entries.sort() # Most recent last.
     
+    lib = get_library_or_404(settings.SPREADLINKS_DIR, 'spreadsite')
+    links = lib.all_links
+    
     entry = entries[-1]
     y, m, d, slug, suffix, dir_name, file_name = entry
     with open(os.path.join(dir_name, file_name), 'rb') as input:
@@ -27,6 +35,7 @@ def entry_list(request, root_dir):
     template_args = {
         'title': msg['title'],
         'body': msg.get_payload(),
+        'links': links,
     }        
     
     return render_to_response('downblog/entry_list.html', template_args, RequestContext(request))
