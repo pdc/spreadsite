3d9b3b1173db28817e2eff84690f118f2eae8316
pdc <pdc@alleged.org.uk>
2010-02-10 09:31:09 +0000

    Split the function for getting alibrary in to models.py

--- spreadlinks/models.py
+++ spreadlinks/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.core.cache import cache
+from django.http import Http404
+from spreadsite.spreadlinks.linklibrarylib import *
+
+def get_library_set(root_dir):
+    """Find the libraries in the specified file-system directoiry."""
+    key = 'library_set'
+    result = cache.get(key)
+    if not result:
+        result = LibrarySet(root_dir)
+        cache.set(key, result)
+    return result
+    
+def get_library_or_404(root_dir, library_name):
+    """Given a parent direcgtory and library name, find the library or throw an HTTP exception."""
+    try:
+        return get_library_set(root_dir)[library_name]
+    except KeyError, e:
+        raise Http404()
+        
 
 # Create your models here.
