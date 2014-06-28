ecd0c8d58071245ecdb9681494aee181f119d37c
pdc <pdc@alleged.org.uk>
2014-06-28 20:17:22 +0100

    Remove redundant "markup" reference

--- spreadsite/settings_production.py
+++ spreadsite/settings_production.py
@@ -93,7 +93,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.admin',
-    'django.contrib.markup',
     'django.contrib.staticfiles',
 
     'downblog',
